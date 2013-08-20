550fc3a6b1d7435f44c2ff168306fcf2657966fa
danecjensen <danecjensen@gmail.com>
2013-08-20 10:52:33 -0500

    rails_apps_composer: testing framework

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ end
 
 module Wanderlust
   class Application < Rails::Application
+
+    # don't generate RSpec tests for views and helpers
+    config.generators do |g|
+      
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      
+      
+      g.view_specs false
+      g.helper_specs false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
