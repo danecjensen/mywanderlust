2c0768ea42f496e6cf15292150531e8c3341e868
danecjensen <danecjensen@gmail.com>
2013-08-29 17:23:16 -0500

    recompile again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Wanderlust::Application.configure do
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
+  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
