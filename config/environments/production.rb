1d1e5f81e8e712c1145a954bdae090d9af3aee4b
danecjensen <danecjensen@gmail.com>
2013-10-31 11:40:36 -0500

    using cloudfront now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,7 +7,7 @@ Wanderlust::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://d2p0uyxtg98ymi.cloudfront.net"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
